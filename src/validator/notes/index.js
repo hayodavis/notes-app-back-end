9a1e26814cb700f3b4367d2512b5db2a85813874
Davis Nur Iqbal Rifandi <davis.fandi@gmail.com>
2021-08-17 05:46:58 +0700

    data validation implemented

---
+++ src/validator/notes/index.js
@@ -0,0 +1,13 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { NotePayLoadSchema } = require("./schema");
+
+const NotesValidator = {
+    validateNotePayload: (payload) => {
+        const validationResult = NotePayLoadSchema.validate(payload);
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
+};
+
+module.exports = NotesValidator;
