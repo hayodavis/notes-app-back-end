609682bec32b4c18baff71fa1d18321fdb9a71df
Davis Nur Iqbal Rifandi <davis.fandi@gmail.com>
2021-09-17 10:50:27 +0700

    add foreign key to notes owner column

--- migrations/1630650725508_create-table-users.js
+++ migrations/1630650725508_create-table-users.js
@@ -6,7 +6,7 @@ exports.up = (pgm) => {
     pgm.createTable('users', {
         id: {
             type: 'VARCHAR(50)',
-            primarykey: true,
+            primaryKey: true,
         },
         username: {
             type: 'VARCHAR(50)',
